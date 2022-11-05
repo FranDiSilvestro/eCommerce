9b6be4bb6c3bca4f2d7df4e46c5045cd675010aa
franco di silvestro <fdisilvestro@dc.uba.ar>
2022-11-04 23:36:54 -0300

    Header Css + Html Update

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 Building a amazon like website to sharpen my frontend skills :).
 
+https://www.youtube.com/watch?v=RDV3Z1KCBvo&t=2226s
+
 Min 37. Primera sesion
+
--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 
 function Header() {
     return (
         <div className="header">
-            <img className="header__logo" src="#" alt="Website Logo" />
+            <img className="header__logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1920px-Amazon_logo.svg.png" alt="Website Logo" />
             <div className="header__search">
                 <input className="header__searchInput" type="text" />
-                {/* Logo */}
+                <SearchIcon className="header__searchIcon" />
             </div>
             <div className="header__nav">
                 <div className="header__option">
@@ -21,6 +23,10 @@ function Header() {
                 <div className="header__option">
                     <span className="header__optionLine1">Your</span>
                     <span className="header__optionLine2">Prime</span>
+                </div>
+                <div className="header__optionBasket">
+                    <ShoppingBasketIcon />
+                    <span className="header__optionLine2 header__basketCount">0</span>
                 </div>
             </div>
         </div>
