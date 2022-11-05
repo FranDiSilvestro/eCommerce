1604441c01bae76c35f2e4e55a12745a820b7ecb
franco di silvestro <fdisilvestro@dc.uba.ar>
2022-11-05 10:59:10 -0300

    Added products and some styles

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ https://www.youtube.com/watch?v=RDV3Z1KCBvo&t=2226s
 
 Min 37. Primera sesion
 Min 1:01. Segunda sesion
-
+Min 1:22. Tercera sesion
--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import "./Home.css"
+import Product from './Product';
 
 function Home() {
   return (
       <div className="home">
           <div className="home__container">
               <img className="home__image" src="https://wallpaperaccess.com/full/3296133.png" alt="Amazon prime" />
+              <div className="home__row">
+                  <Product />
+                  {/* Product */}
+                  <Product />
+                  {/* Product */}
+              </div>
+              <div className="home__row">
+                  <Product />
+                  {/* Product */}
+                  <Product />
+                  {/* Product */}
+                  <Product />
+                  {/* Product */}
+              </div>
+              <div className="home__row">
+                  <Product />
+                  {/* Product */}
+              </div>
           </div>
       </div>
   );
